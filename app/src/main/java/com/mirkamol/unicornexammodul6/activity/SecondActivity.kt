26a490d0367bd933522f506abb3ace41958eb286
Mirkamol <mirkamol9187907@gmail.com>
2022-03-17 12:13:27 +0500

    takes from server

--- app/src/main/java/com/mirkamol/unicornexammodul6/activity/SecondActivity.kt
+++ app/src/main/java/com/mirkamol/unicornexammodul6/activity/SecondActivity.kt
@@ -2,11 +2,20 @@ package com.mirkamol.unicornexammodul6.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.mirkamol.unicornexammodul6.R
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var ivCancel:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        ivCancel = findViewById(R.id.ivCancel)
+
+        ivCancel.setOnClickListener {
+            finish()
+        }
+
     }
 }
